8881d2a444c5fa8cf0716e068f5c46c720c84eac
K-Sh-n <kishennakrani@gmail.com>
2023-02-11 20:23:57 +0000

    Added testing class for MovInstruction class

--- test/sml/instruction/MovInstructionTest.java
+++ test/sml/instruction/MovInstructionTest.java
@@ -1,2 +1,46 @@
-package sml.instruction;public class MovInstructionTest {
+package sml.instruction;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import static sml.Registers.Register.EAX;
+
+class MovInstructionTest {
+    private Machine machine;
+    private Registers registers;
+
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        //...
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
+
+    @Test
+    void executeValid() {
+        Instruction instruction = new MovInstruction(null, EAX, 5);
+        instruction.execute(machine);
+        Assertions.assertEquals(5, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidTwo() {
+        Instruction instruction = new MovInstruction(null, EAX, 5);
+        Instruction instruction2 = new MovInstruction(null, EAX, 15);
+        instruction.execute(machine);
+        instruction2.execute(machine);
+        Assertions.assertEquals(15, machine.getRegisters().get(EAX));
+    }
 }
