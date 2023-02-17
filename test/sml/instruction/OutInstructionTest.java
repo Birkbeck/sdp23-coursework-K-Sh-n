aa6c28f7839811db19d288044042bc436d957975
K-Sh-n <kishennakrani@gmail.com>
2023-02-17 11:45:09 +0000

    Created OutinstructionTest.java

--- test/sml/instruction/OutInstructionTest.java
+++ test/sml/instruction/OutInstructionTest.java
@@ -1,2 +1,40 @@
-package sml.instruction;public class OutInstructionTest {
+package sml.instruction;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import static sml.Registers.Register.*;
+
+class OutInstructionTest {
+    private Machine machine;
+    private Registers registers;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        //...
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
+
+    @Test
+    void printOut() {
+        registers.set(EBX, 6);
+        Instruction instruction = new OutInstruction(null,EBX);
+        instruction.execute(machine);
+        String string1 = "out EBX";
+        String string2 = instruction.toString();
+        Assertions.assertEquals(string1, string2);
+    }
 }
